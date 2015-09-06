661eb99a3cba503ec21257d6e4d61c241a9096f3
Vikas Gorur <vikasgp@gmail.com>
2015-09-05 18:02:38 -0700

    Simplify bigger-is-greater solution.

--- bigger-is-greater/solution.py
+++ bigger-is-greater/solution.py
@@ -4,13 +4,9 @@ def lex_next(w):
     splits = ((w[0:i], w[i:]) for i in range(len(w)-1, 0, -1))
 
     for (s, t) in splits:
-        greater_chars = [c for c in t if c > s[-1]]
-        if len(greater_chars) > 0:
-            replacement = min(greater_chars)
-            t_list = list(t)
-            t_list[t.find(replacement)] = s[-1]
-
-            return s[:-1] + replacement + "".join(sorted(t_list))
+        sub = min([c for c in t if c > s[-1]], default=False)
+        if not sub: continue
+        return s[:-1] + sub + "".join(sorted(t.replace(sub, s[-1], 1)))
 
     return "no answer"
 
